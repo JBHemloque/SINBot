6606df721e2584bea5ba176d9113b17525d29360
Jim Anderson <jim.anderson@hbo.com>
2016-02-29 14:45:08 -0800

    Added help

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var dice = require('./dice.js');
 var search = require('./search.js');
 var config = require('./config.js');
 
-const VERSION = "SINBot Version 0.1.0";
+const VERSION = "SINBot Version 0.3.0";
 
 var SINBot = new Discord.Client();
 
@@ -12,13 +12,43 @@ var compileArgs = function(args) {
 	return args.join(" ");
 }
 
+var genCommand = function(fcnName, fcnHelp, fcnFunction) {
+	return {
+		name: fcnName,
+		help: fcnHelp,
+		func: fcnFunction
+	};
+}
+
 var simpleCommands = {}
-simpleCommands["!pat"] = function(args, callback) { callback("There, there, " + compileArgs(args)); }
-simpleCommands["!roll"] = function(args, callback) { callback(dice.rollDice(compileArgs(args))); }
-simpleCommands["!version"] = function(args, callback) { callback(VERSION); }
+simpleCommands["!pat"] = genCommand("pat [name]", "Rough day? Comfort someone with this command.",
+	function(args, callback) { callback("There, there, " + compileArgs(args)); });
+simpleCommands["!roll"] = genCommand("roll [dice]", "Dice rolling command. Supports standard dice notation, including F-5/Fudge dice (e.g.: 4dF+2).",
+	function(args, callback) { callback(dice.rollDice(compileArgs(args))); });
+simpleCommands["!version"] = genCommand("version", "Display version information for this bot.", 
+	function(args, callback) { callback(VERSION); });
+simpleCommands["!help"] = genCommand("help", "Display help for this bot.", 
+	function(arg, callback) {
+		var output = "SINBot commands:";
+		var key;
+		for (key in simpleCommands) {
+			output += "\n\t!";
+			output += simpleCommands[key].name;
+			output += "\n\t\t\t";
+			output += simpleCommands[key].help;
+		}
+		for (key in complexCommands) {
+			output += "\n\t!";
+			output += complexCommands[key].name;
+			output += "\n\t\t\t";
+			output += complexCommands[key].help;
+		}
+		callback(output);
+	});
 
 var complexCommands = {}
-complexCommands["!precis"] = function(args, bot, message) { search.precis(compileArgs(args), bot, message); }
+complexCommands["!precis"] = genCommand("precis [name]", "Generate a precis on someone.",
+	function(args, bot, message) { search.precis(compileArgs(args), bot, message); });
 
 
 SINBot.on("message", function(message){
@@ -29,13 +59,13 @@ SINBot.on("message", function(message){
 			var args = message.content.split(" ");
 			var command = simpleCommands[args[0]];
 			if (command) {
-				command(args, function(resp) {
+				command.func(args, function(resp) {
 					SINBot.sendMessage(message.channel, resp);
 				});
 			} else {
 				command = complexCommands[args[0]];
 				if (command) {
-					command(args, SINBot, message);			
+					command.func(args, SINBot, message);			
 				}
 			}
 		}
