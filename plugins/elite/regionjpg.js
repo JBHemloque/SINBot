cd89fcfafcb731a295294dd93c6f5003a64c90ec
JBHemloque <=>
2016-04-22 13:18:39 -0700

    Cleaned up parse/mkmaps

--- plugins/elite/regionjpg.js
+++ plugins/elite/regionjpg.js
@@ -4,16 +4,22 @@ var fs = require("fs");
 var gm = require('gm');
 var imageMagick = gm.subClass({ imageMagick: true });
 
-var regions = require("./regions.json");
+var _regions = require("./regions.json");
 
 // Magic numbers based on the source image, Galaxy.jpg
 const KLY_TO_PIXEL = 1000 / 12.5;
 const X0 = 500;
 const Y0 = 875;
 
-var regionFont = "Wingdings";
+var _regionFont = "ArialBk";
+var _sourceFile = "./plugins/elite/Galaxy.jpg";
+var _destDir = "./plugins/elite/maps/";
 
-var generateRegionMap = function(key, callback) {
+var generateRegionMap = function(key, callback, regionsOverride) {
+	var regions = _regions;
+	if (regionsOverride) {
+		regions = regionsOverride;
+	}
 	var x = regions[key].coords.x;
 	// coords.z is actually the y axis on our maps. Blame EDSM...
 	var y = regions[key].coords.z;
@@ -26,9 +32,9 @@ var generateRegionMap = function(key, callback) {
 
 	// convert Galaxy.jpg -fill white -stroke black -draw "circle Circle.x,Circle.y Circle.x+5,Circle.y+5" 
 	// -fill white -stroke black -font ArialBk -pointsize 20 -draw "text Circle.x+20,Circle.y+7 'Hello'" maps/REGION.jpg
-	imageMagick("./plugins/elite/Galaxy.jpg").fill("#ffffffff").stroke("#000000ff").drawCircle(x,y,x+5,y+5)
-				.font(regionFont).fontSize(20).drawText(x+20,y+7,regions[key].region)
-				.write("./plugins/elite/maps/" + key + ".jpg", function(err) {
+	imageMagick(_sourceFile).fill("#ffffffff").stroke("#000000ff").drawCircle(x,y,x+5,y+5)
+				.font(_regionFont).fontSize(20).drawText(x+20,y+7,regions[key].region)
+				.write(_destDir + key + ".jpg", function(err) {
 		if (err) {
 			console.log("Error doing conversion: ");
 			console.log(err);
@@ -42,18 +48,26 @@ var generateRegionMap = function(key, callback) {
 
 var fetchRegionMap = function(region, callback) {
 	var key = region.toLowerCase();
-	if (regions[key].map) {
+	if (_regions[key].map) {
 		callback();
 	} else {
 		generateRegionMap(key, function() {
-			fs.writeFile("./regions.json",JSON.stringify(regions,null,2), null);
+			fs.writeFile("./regions.json",JSON.stringify(_regions,null,2), null);
 			callback();
 		});
 	}		
 }
 
 var setRegionFont = function(font) {
-	regionFont = font;
+	_regionFont = font;
+}
+
+exports.setSourceFile = function(sourceFile) {
+	_sourceFile = sourceFile;
+}
+
+exports.setDestDir = function(destDir) {
+	_destDir = destDir;
 }
 
 exports.setRegionFont = setRegionFont;
