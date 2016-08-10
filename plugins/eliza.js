e4c2b3de9a554d200a18bad02e2f37499a85a78a
Jim-Anderson <Jim.Anderson@hbo.com>
2016-08-10 16:18:31 -0700

    Experimental Eliza with external data, allowing us to create new instances with different rulesets.

--- plugins/eliza.js
+++ plugins/eliza.js
@@ -4,22 +4,14 @@ var utils = require('../utils.js');
 var elizabot = require('./elizabot.js');
 
 var commands = {
-	"elizastart": {
-		help: "Start a new conversation",
-		process: function(args, bot, message) { startEliza(args, bot, message); }
-	},
-	"elizabye": {
-		help: "Done with your conversation with the bot",
-		process: function(args, bot, message) { endEliza(args, bot, message); }
-	},
 	"eliza": {
 		usage: "<anything - just talk>",
 		help: "Let's talk...",
 		process: function(args, bot, message) {
-			if (!elizaStarted) {
-				elizaStarted = true;
+			if (!eliza) {
+				eliza = new ElizaBot("./elizadata.js", false);
 			}
-			bot.sendMessage(message.channel, elizabot.reply(utils.compileArgs(args)));
+			bot.sendMessage(message.channel, eliza.transform(utils.compileArgs(args)));
 		}
 	},
 };
@@ -30,17 +22,4 @@ exports.findCommand = function(command) {
 
 exports.commands = commands;
 
-var elizaStarted = false;
-
-var startEliza = function(args, bot, message) {
-	if (elizaStarted) {
-		bot.sendMessage(message.channel, elizabot.bye());
-	}
-	bot.sendMessage(message.channel, elizabot.start());
-	elizaStarted = true;
-}
-
-var endEliza = function(args, bot, message) {
-	bot.sendMessage(message.channel, elizabot.bye());
-	elizaStarted = false;
-}
+var eliza;
