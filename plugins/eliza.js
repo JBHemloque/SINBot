21a1014cda90ff549fb5c256ad898cd7c53d1c80
Jim Anderson <jim.anderson@hbo.com>
2016-08-17 22:43:40 -0700

    Reverted experimental Eliza/Jaques

--- plugins/eliza.js
+++ plugins/eliza.js
@@ -4,14 +4,22 @@ var utils = require('../utils.js');
 var elizabot = require('./elizabot.js');
 
 var commands = {
+	"elizastart": {
+		help: "Start a new conversation",
+		process: function(args, bot, message) { startEliza(args, bot, message); }
+	},
+	"elizabye": {
+		help: "Done with your conversation with the bot",
+		process: function(args, bot, message) { endEliza(args, bot, message); }
+	},
 	"eliza": {
 		usage: "<anything - just talk>",
 		help: "Let's talk...",
 		process: function(args, bot, message) {
-			if (!eliza) {
-				eliza = new ElizaBot("./elizadata.js", false);
+			if (!elizaStarted) {
+				elizaStarted = true;
 			}
-			bot.sendMessage(message.channel, eliza.transform(utils.compileArgs(args)));
+			bot.sendMessage(message.channel, elizabot.reply(utils.compileArgs(args)));
 		}
 	},
 };
@@ -22,4 +30,17 @@ exports.findCommand = function(command) {
 
 exports.commands = commands;
 
-var eliza;
+var elizaStarted = false;
+
+var startEliza = function(args, bot, message) {
+	if (elizaStarted) {
+		bot.sendMessage(message.channel, elizabot.bye());
+	}
+	bot.sendMessage(message.channel, elizabot.start());
+	elizaStarted = true;
+}
+
+var endEliza = function(args, bot, message) {
+	bot.sendMessage(message.channel, elizabot.bye());
+	elizaStarted = false;
+}
--- plugins/jaques.js
+++ plugins/jaques.js
@@ -1,17 +1,25 @@
 'use strict';
 
 var utils = require('../utils.js');
-var elizabot = require('./elizabot.js');
+var jaquesbot = require('./jaquesbot.js');
 
 var commands = {
+	"jaquesstart": {
+		help: "Jaques is your cyborg bartender. Have a drink!",
+		process: function(args, bot, message) { startjaques(args, bot, message); }
+	},
+	"jaquesbye": {
+		help: "It's closing time, you can't drink here...",
+		process: function(args, bot, message) { endjaques(args, bot, message); }
+	},
 	"jaques": {
 		usage: "<anything - just talk>",
-		help: "Let's talk...",
+		help: "I'm Jaques, your cyborg bartender. Let's talk...",
 		process: function(args, bot, message) {
-			if (!eliza) {
-				eliza = new ElizaBot("./jaquesdata.js", false);
+			if (!jaquesStarted) {
+				startjaques(args, bot, message);
 			}
-			bot.sendMessage(message.channel, eliza.transform(utils.compileArgs(args)));
+			bot.sendMessage(message.channel, jaquesbot.reply(utils.compileArgs(args)));
 		}
 	},
 };
@@ -22,4 +30,17 @@ exports.findCommand = function(command) {
 
 exports.commands = commands;
 
-var eliza;
+var jaquesStarted = false;
+
+var startjaques = function(args, bot, message) {
+	if (jaquesStarted) {
+		bot.sendMessage(message.channel, jaquesbot.bye());
+	}
+	bot.sendMessage(message.channel, jaquesbot.start());
+	jaquesStarted = true;
+}
+
+var endjaques = function(args, bot, message) {
+	bot.sendMessage(message.channel, jaquesbot.bye());
+	jaquesStarted = false;
+}
