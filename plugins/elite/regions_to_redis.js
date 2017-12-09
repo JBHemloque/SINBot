07f69511e4e3b12129e908f7d4b21c88be3b3515
Jim Anderson <jbhemlock@gmail.com>
2017-12-08 16:35:49 -0800

    Made this convenient

--- plugins/elite/regions_to_redis.js
+++ plugins/elite/regions_to_redis.js
@@ -4,29 +4,33 @@ var base = require('../base.js');
 var path = require('path');
 var regions = require('./regions');
 
-function regionsToRedis(regionfile) {
-	var json = require(regionfile);
+function regionsToRedis(regionfile) {	
+	try{
+	    console.log("Attempting to load regionfile " + regionfile);
+	    var json = require(regionfile);
+	    var count = 0;
+		var thousands = 0;
 
-	var count = 0;
-	var thousands = 0;
-
-	if (json) {
-		console.log('writing...');
-		for (var key in json) {
-			var rgn = json[key];
-			if (rgn) {
-				count += 1;
-				if (count >= 1000) {
-					thousands += 1;
-					console.log(thousands + ' thousand...');
-					count = 0;
+		if (json) {
+			console.log('writing...');
+			for (var key in json) {
+				var rgn = json[key];
+				if (rgn) {
+					count += 1;
+					if (count >= 1000) {
+						thousands += 1;
+						console.log(thousands + ' thousand...');
+						count = 0;
+					}
+					regions.writeRegionToRedis(rgn);
 				}
-				regions.writeRegionToRedis(rgn);
 			}
+			console.log('Done! Wrote ' + thousands + ',' + count + ' regions.');
+		} else {
+			console.log("Could not load " + regionfile);
 		}
-		console.log('Done! Wrote ' + thousands + ',' + count + ' regions.');
-	} else {
-		console.log("Could not load " + regionfile);
+	} catch(e) {
+	    console.log("Could not load regions...");
 	}
 }
 module.exports.regionsToRedis = regionsToRedis;
