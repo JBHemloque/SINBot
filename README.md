a05c7abbd7a3ba2da60b9a6be4be0a064044d63c
Jim Anderson <jbhemlock@gmail.com>
2025-03-15 23:45:22 -0700

    Updates

--- README.md
+++ README.md
@@ -3,29 +3,16 @@
 A chat bot for discord app based off <a href="https://github.com/hydrabolt/discord.js/">discord.js</a>
 
 # Features:
-- Newly updated to discord.js version 11, with support for bot accounts and token-based authentication.
-- Plugin architecture for easy customization
-- Easy to deploy
-
-# Plugins:
-- dice - a dice roller, including Fudge dice
-- elite - routines useful for explorers in the game Elite Dangerous, including edsm integration and the ability to generate simple galactic maps
-- eliza - an elizabot
-- jaques - a RiveScript bot for Jaques, cybernetic bartender in the game Elite Dangerous
-- randomalias - display random categorized results ("random cat", etc)
-- rss - rss feed agent with channel notification ("A new article has appeared")
-- search - a custom plugin for the Final Fantasy XIV guild "The Black Coeurl" to enable searching for characters and generating a precis
-- trello - trello agent with channel notification ("So-and-so edited the card blah blah")
-- wolfram - wolfram alpha integration
+- Newly updated to discord.js version 14, with support for bot accounts and token-based authentication.
 
 # Installation
 
-This bot is written to run on top of node.js version 6. Please see https://nodejs.org/en/download/
+This bot is written to run on top of node.js version 22. Please see https://nodejs.org/en/download/
 
 Once you have node installed running `npm install` from the bot directory should install all the needed packages. If this command prints errors the bot won't work!
 
 # Running
-Before first run you will need to create a `config.js` file. The token for a discord bot account are required. The other credentials are not required for the bot to run, but highly recommended as commands that depend on them will malfunction. See `sample_config.js`.
+Before first run you will need to create a `config.json` file. The token for a discord bot account are required. The other credentials are not required for the bot to run, but highly recommended as commands that depend on them will malfunction. See `sample_config.json`.
 
 To start the bot just run
 `node server/server.js`.
@@ -36,37 +23,3 @@ The bot doesn't include an uncaught exception handler, so I highly recommend run
 If you update the bot, please run `npm update` before starting it again. If you have
 issues with this, you can try deleting your node_modules folder and then running
 `npm install` again. Please see [Installation](#Installation).
-
-This bot includes code from GreenImp's JavaScript RPG dice roller: https://github.com/GreenImp/rpg-dice-roller
-
-# Special configuration
-
-## Special instructions for setting up google search APIs:
-
-(thanks @SchwererKonigstiger)
-
-1) Create a Custom Search at: https://cse.google.com/cse/create/new
-
-2) Leave the first line blank, and name the search engine anything you wish.
-
-3) Click "Advanced Options" and then type ImageObject.
-
-4) Hit create.
-
-5) On this new page, enable the Image Search in the menu.
-
-6) Then press "Search engine ID" under the Details header.
-
-7) Copy this into the auth.json's "google_custom_search" section.
-
-Make sure you also have your google server API key or the search will fail.
-
-## Special instructions for setting up the Elite plugin. 
-
-The Elite plugin uses ImageMagick to generate the region maps from data dumped from edsm. In order to have accurate maps, you will need to have ImageMagick installed (https://www.imagemagick.org/script/index.php) and a periodic process to grab data from edsm. In addition, the Galactic Mapping Project data should be periodically refreshed in order for it to be up to date.
-
-1) Install ImageMagick from https://www.imagemagick.org/script/index.php and ensure it is on your path. If you fail to do this, you will see errors from the region commands (showloc, region, etc) in the bots error logs.
-
-2) Write a scheduled task to download and refresh the region data. A sample is included in this repo (refreshregions.sh). This script should download https://www.edsm.net/dump/systemsWithCoordinates.json, stop the bot, run plugins/elite/makeregions (to process the system data into a region database), and then restart the bot. The plugin will fall back to a backup region file if it can't find/load the primary file, and the sample script creates this.
-
-3) Write a scheduled task to download and refresh the galactic mapping project data. There is a shell script, plugins/elite/refreshgmp, which does this. This isn't critical, since the GMP data refresh is fast, and it happens automatically when you start the bot, so it will happen as a side effect of the region data refresh.
--- server/server.js
+++ server/server.js
@@ -1,102 +1,60 @@
 'use strict';
 
-var bot = require('./bot.js');
-var Discord = require("discord.js");
-var config = require('../config.js');
-var utils = require('./utils.js');
-var base = require('../base.js');
-var healthcheck = require('./healthcheck.js');
+const { Client, Collection, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+var config = require('../config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var healthcheck = require('./healthcheck.js');
 
-var options = {};
-if (config.DISCORD_OPTIONS) {
-    options = config.DISCORD_OPTIONS;
-}
-console.log("Using discord options: " + JSON.stringify(options));
-var SINBot = new Discord.Client(options);
+const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
-console.log('Bot base directory: ' + base.path);
+client.commands = new Collection();
 
-const eventsPath = path.join(__dirname, 'events');
+const foldersPath = path.join(__dirname, 'commands');
+console.log(`foldersPath: ${foldersPath}`);
+const commandFolders = fs.readdirSync(foldersPath);
 
-console.log('Events path: ' + eventsPath);
-
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        SINBot.once(event.name, (...args) => event.execute(...args));
-    } else {
-        SINBot.on(event.name, (...args) => event.execute(...args));
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            console.log(`Setting up ${command.data.name}...`);
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
 }
 
-SINBot.on("message", function(message){
-    bot.procCommand(SINBot, message);
+client.once(Events.ClientReady, readyClient => {
+    healthcheck.startHealthCheck(this);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-//Log user status changes
-SINBot.on(Events.PresenceUpdate, function(user,status,gameId) {
-    bot.procPresence(SINBot, user, status, gameId);
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        }
+    }
 });
 
-SINBot.on('disconnected', function() {
-    utils.logError("Disconnected", "Attempting restart...");
-    startBot();
-});
-
-function startBot() {
-    healthcheck.startHealthCheck(SINBot);
-    SINBot.login(config.TOKEN)
-        .then(atoken => {
-            console.log('logged in with token ' + atoken);
-            try {
-                bot.startBot(SINBot, config, function() {
-                    console.log("Bot initialization complete!");
-                });
-            } catch (e) {
-                utils.logError("startBot error", e);
-            }
-        })
-        .catch(console.error);
-}
-
-function consoleBotInput(consoleBot) {
-    readline.question(`Bot: `, (input) => {
-  
-      var message = {
-        author: {username: 'User'},
-        channel: 'Channel',
-        sender: {username: 'Sender'},
-        content: input,
-        isMentioned: function(user) { return false; }
-    };
-
-        bot.procCommand(consoleBot, message);
-
-        // readline.close();
-        consoleBotInput(bot);
-    });
-}
-
-function consoleBot() {
-    var botUser = {username: 'Console Bot'};
-    var consoleBot = {user: botUser};
-    bot.startBot(SINBot, config, function() {
-        console.log("Bot initialization complete!");
-        consoleBotInput(consoleBot);
-    });
-}
-
-if (config.CONSOLE) {
-    consoleBot();
-} else {
-    startBot();
-}
+client.login(config.token);
